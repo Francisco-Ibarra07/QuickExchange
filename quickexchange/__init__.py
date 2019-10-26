fc8c88ec283585a729ac5782576d4e945791e759
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-10-26 14:05:38 -0700

    Created new routes for logout and account

--- quickexchange/__init__.py
+++ quickexchange/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -7,6 +9,10 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # <-- function name of our login route
+login_manager.login_message_category = 'info'
 
 # Import here to avoid circular imports
 from quickexchange import routes
--- quickexchange/routes.py
+++ quickexchange/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, url_for, flash, redirect
-from quickexchange import app, db
-from quickexchange.forms import RegistrationForm, LoginForm, URLSetterForm
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from quickexchange import app, bcrypt, db
+from quickexchange.forms import RegistrationForm, LoginForm, UpdateAccountForm, URLSetterForm
 from quickexchange.models import User, Post, URLPost
-
-url_stack = []
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 def root():
@@ -41,22 +43,84 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit(): # This tells us if our form was validated on submit
-        flash(f'Account created for {form.username.data}', 'success') # Displays a message at the top 
-        return redirect(url_for('home')) # Then we redirect the user to home page
+
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # Decode() returns us a string
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Your account has been created! You are now able to log in', 'success') # Displays a message at the top 
+        return redirect(url_for('login')) # Then we redirect the user to home page
 
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
-    # If form was submitted correctly
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next') # <-- use get() to access args dictionary so that if it DNE, it throws None
+            
+            if next_page is not None:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+# Randomizes picture file name to avoid collisions. Then saves image in db
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8) 
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+    
+    # Resize img using Pillow module
+    # output_size = (125, 125)
+    # i = Image.open(form_picture)
+    # i.thumbnail(output_size)
+    # i.save(picture_path)
+
+    form_picture.save(picture_path)
+
+    return picture_filename
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account')) # <-- POST GET redirect pattern. Prevents browser giving that warning that you will POST again
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
