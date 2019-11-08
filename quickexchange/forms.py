66b9dc2c227e08688bb4f0386074d61e319a0ddd
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-11-07 21:41:14 -0800

    Added ValidationError class

--- quickexchange/forms.py
+++ quickexchange/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, URL
+from wtforms.validators import DataRequired, Length, Email, EqualTo, URL, ValidationError
 from quickexchange.models import User
 from flask_login import current_user
 
