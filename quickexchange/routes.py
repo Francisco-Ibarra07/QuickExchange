f047c7919f6e359ae8373a0f304b925856da455f
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-11-07 22:02:19 -0800

    Moved history to just be on Home page

--- quickexchange/routes.py
+++ quickexchange/routes.py
@@ -140,4 +140,4 @@ def account():
         form.email.data = current_user.email
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file, form=form, history=reversed(current_user.posts))
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
