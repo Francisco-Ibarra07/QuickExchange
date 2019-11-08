869d41355afc20042c1c5933f9624f901e20e911
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-11-07 21:51:19 -0800

    Removed new post link

--- quickexchange/routes.py
+++ quickexchange/routes.py
@@ -141,15 +141,3 @@ def account():
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form, history=reversed(current_user.posts))
-
-@app.route('/post/new', methods=['GET', 'POST'])
-@login_required
-def create_post():
-    form = PostForm()
-
-    if form.validate_on_submit():
-        flash('Post has been created!', 'success')
-        return redirect(url_for('home'))
-
-    return render_template('create-post.html', title='New Post', form=form)
-
