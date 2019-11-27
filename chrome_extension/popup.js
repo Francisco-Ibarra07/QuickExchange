dd9b165b1609bd37215086fed22270014a63742c
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-11-26 20:38:54 -0800

    Added async function to populate latest link

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -3,7 +3,32 @@ console.log("Popup.js running");
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("copyButton").addEventListener("click", copyButtonClickHandler);
   document.getElementById("popButton").addEventListener("click", popButtonClickHandler);
+  populateTextAreaElement();
 });
+
+// Fetch latest post url from Flask route
+async function populateTextAreaElement() {
+  try {
+    const TEST_EMAIL = 'hmd@m.com';
+    const response = await fetch('http://127.0.0.1:8080/get-latest-post?email=' + TEST_EMAIL);
+    const data = await response.json();
+
+    console.log("Data recieved:");
+    console.log(JSON.stringify(data));
+
+    // Set textarea value to fetched url
+    if(data.hasOwnProperty('url')) {
+      console.log('url property: ', data['url']);
+      let textareaElement = document.getElementById("currentStoredMediaBox");
+      textareaElement.value = data['url'];
+    } else {
+      console.log('url property not found in: ', data);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function copyButtonClickHandler(event) {
   let textareaElement = document.getElementById("currentStoredMediaBox");
