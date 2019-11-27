d42d756c83560565c8ea7abc31c343dc78997ec4
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-11-27 10:27:03 -0800

    Created async function to do post request to store url input

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,8 +1,9 @@
 console.log("Popup.js running");
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("copyButton").addEventListener("click", copyButtonClickHandler);
   document.getElementById("popButton").addEventListener("click", popButtonClickHandler);
+  document.getElementById("pushButton").addEventListener("click", pushButtonClickHandler);
   populateTextAreaElement();
 });
 
@@ -13,12 +14,12 @@ async function populateTextAreaElement() {
     const response = await fetch('http://127.0.0.1:8080/get-latest-post?email=' + TEST_EMAIL);
     const data = await response.json();
 
-    console.log("Data recieved:");
-    console.log(JSON.stringify(data));
+    // console.log("Data recieved:");
+    // console.log(JSON.stringify(data));
 
     // Set textarea value to fetched url
-    if(data.hasOwnProperty('url')) {
-      console.log('url property: ', data['url']);
+    if (data.hasOwnProperty('url')) {
+      // console.log('url property: ', data['url']);
       let textareaElement = document.getElementById("currentStoredMediaBox");
       textareaElement.value = data['url'];
     } else {
@@ -32,16 +33,16 @@ async function populateTextAreaElement() {
 
 function copyButtonClickHandler(event) {
   let textareaElement = document.getElementById("currentStoredMediaBox");
-  
+
   // Copy text to keyboard
   navigator.clipboard.writeText(textareaElement.value)
-  .then(() => {
-    console.log('Text copied to clipboard');
-  })
-  .catch(err => {
-    // This can happen if the user denies clipboard permissions:
-    console.error('Could not copy text: ', err);
-  });
+    .then(() => {
+      console.log('Text copied to clipboard');
+    })
+    .catch(err => {
+      // This can happen if the user denies clipboard permissions:
+      console.error('Could not copy text: ', err);
+    });
 }
 
 function popButtonClickHandler(event) {
@@ -57,3 +58,57 @@ function popButtonClickHandler(event) {
   var win = window.open(url, '_blank');
   win.focus();
 }
+
+// Quick function to test if a string is a url
+const isValidUrl = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;  
+  }
+}
+
+// Will validate data and will call an async function to make the post request
+async function pushButtonClickHandler(event) {
+  const urlInput = document.getElementById("urlInput").value;
+  if (urlInput === '') {
+    console.log("No url input: ", urlInput);
+    return;
+  }
+
+  // Validate URL
+  if(!isValidUrl(urlInput)) {
+    console.log("Invalid URL");
+    return;
+  }
+  
+  console.log("Sending url input: ", urlInput);
+  try {
+    // Create json data for post request
+    // TODO(FI): Replace test email with actual email
+    const TEST_EMAIL = "hmd@m.com";
+    const dataToSend = {
+      "url": urlInput,
+      "email": TEST_EMAIL
+    };
+
+    // Create post request
+    const response = await fetch('http://127.0.0.1:8080/set-post', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataToSend)
+    });
+    
+    // Wait for data 
+    const responseData = await response.json();
+    console.log("Response after fetch(): ", responseData);
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
