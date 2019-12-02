088fffb73eab15b75e79261f617e54f99991bae4
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-12-02 10:51:14 -0800

    Testing CORS allowance

--- quickexchange/__init__.py
+++ quickexchange/__init__.py
@@ -6,11 +6,11 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-CORS(app)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # <-- function name of our login route
@@ -22,4 +22,11 @@ from quickexchange import routes
 @app.before_first_request
 def create_tables():
   db.create_all()
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
   
