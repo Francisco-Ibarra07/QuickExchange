bb9575e20fd9ba24ad4f700b9f22c99d35dafdeb
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2019-12-22 20:41:52 -0800

    Created two forms for the reset password process

--- quickexchange/forms.py
+++ quickexchange/forms.py
@@ -83,3 +83,21 @@ class UpdateAccountForm(FlaskForm):
                     "That email is taken. Please choose another email."
                 )
 
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request password reset")
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email. You must register first.")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
+
